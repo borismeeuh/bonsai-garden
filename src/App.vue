3fc8b13791efdebda83863bc71b6e98424a76446
Boris Kamstra <boriskamstra34@gmail.com>
2025-07-20 23:29:12 +0200

    tailwind opgezet, foto's toegevoegd, smooth scroll toegevoegd

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Backdrop from "./components/Backdrop.vue";
+import Hero from "./components/Hero.vue";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollSmoother.create({
+  smooth: 1,
+  effects: true,
+  smoothTouch: 0.1,
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="smooth-wrapper">
+    <div id="smooth-content">
+      <Backdrop />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Hero />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/Backdrop.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="bg-deep min-h-screen p-12">
+    <div class="space-y-12 max-w-7xl mx-auto">
+      <!-- Rij 1: 33% / 67% -->
+      <div class="flex gap-12 h-1/2">
+        <div class="w-1/3">
+          <img
+            src="/src/images/bonsai-1.jpg"
+            alt="Bonsai 1"
+            class="w-full h-full object-cover shadow-lg"
+          />
+        </div>
+        <div class="w-2/3">
+          <img
+            src="/src/images/bonsai-2.jpg"
+            alt="Bonsai 2"
+            class="w-full h-full object-cover shadow-lg"
+          />
+        </div>
+      </div>
+
+      <!-- Rij 2: 67% / 33% -->
+      <div class="flex gap-12 h-1/2">
+        <div class="w-2/3">
+          <img
+            src="/src/images/bonsai-3.jpg"
+            alt="Bonsai 3"
+            class="w-full h-full object-cover shadow-lg"
+          />
+        </div>
+        <div class="w-1/3">
+          <img
+            src="/src/images/bonsai-4.png"
+            alt="Bonsai 4"
+            class="w-full h-full object-cover shadow-lg"
+          />
+        </div>
+      </div>
+
+      <div class="flex gap-12 h-1/2">
+        <div class="w-1/3">
+          <img
+            src="/src/images/bonsai-5.png"
+            alt="Bonsai 1"
+            class="w-full h-full object-cover shadow-lg"
+          />
+        </div>
+        <div class="w-2/3">
+          <img
+            src="/src/images/bonsai-5.png"
+            alt="Bonsai 2"
+            class="w-full h-full object-cover shadow-lg"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
