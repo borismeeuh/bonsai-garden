432d5476e99c85027879a3131858ce19f8aac703
Boris Kamstra <boriskamstra34@gmail.com>
2025-07-23 01:56:43 +0200

    Tekst toegevoegd

--- src/components/Backdrop.vue
+++ src/components/Backdrop.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="bg-deep min-h-screen p-12 relative" id="background">
+  <section class="bg-deep min-h-screen relative" id="background">
     <div
       class="space-y-12 max-w-7xl mx-auto"
       id="test"
       data-speed="0.7"
       data-lag="0.1"
     >
-      <div class="flex gap-12 h-1/2">
+      <div class="flex gap-12 h-1/2 blur-lg">
         <div class="w-1/3">
           <img
             src="/src/images/bonsai-1.jpg"
@@ -57,7 +57,9 @@
         </div>
       </div>
     </div>
-    <div class="absolute top-75 left-175">test</div>
+    <div class="absolute top-50 w-1/1 text-sand flex justify-center">
+      <div class="text-8xl font-display font-extralight">Bonsai Garden</div>
+    </div>
   </section>
 </template>
 
